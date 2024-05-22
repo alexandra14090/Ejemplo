da298ff73f559810548e134bb7bbc61e9f455604
Proyectos Jessica Giraldo <alexandra.giraldo@utp.edu.co>
2024-05-22 14:57:48 -0500

    Actualización

--- Ejemplo Flyweight/FlyweightPatternDemo.java
+++ Ejemplo Flyweight/FlyweightPatternDemo.java
@@ -1,4 +1,4 @@
-public class FlyweightPatternDemo {
+/*public class FlyweightPatternDemo {
     public static void main(String[] args) {
         EmployeeType devType = EmployeeFactory.getEmployeeType(
             "Developer", "IT", "Health, 401k", "123 Tech Street", "555-1234", "9 AM - 5 PM", "Full-Time",
@@ -11,5 +11,20 @@ public class FlyweightPatternDemo {
 
         EmployeeType emp1Type = devType; 
         System.out.println("Employee Type of emp1: " + emp1Type);
+    }
+}*/
+
+public class FlyweightPatternDemo {
+    public static void main(String[] args) {
+        EmployeeType type1 = EmployeeFactory.getEmployeeType("Developer", "IT", "Health Insurance", "123 Main St", "555-5555",
+                                                             "9 AM - 5 PM", "Full-Time", "2 weeks per year", "Ongoing Training",
+                                                             "Flexible", "Yes", "401k", "2023-01-01", "10 holidays");
+
+        EmployeeType type2 = EmployeeFactory.getEmployeeType("Developer", "IT", "Health Insurance", "123 Main St", "555-5555",
+                                                             "9 AM - 5 PM", "Full-Time", "2 weeks per year", "Ongoing Training",
+                                                             "Flexible", "Yes", "401k", "2023-01-01", "10 holidays");
+
+        // type1 y type2 son la misma instancia
+        System.out.println(type1 == type2); // Salida: true
     }
 }
